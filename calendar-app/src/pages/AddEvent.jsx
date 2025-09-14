87f010389bc2580907267e90c1d6f10ff5130d4f
yeudit <y00556735623@gmail.com>
2025-09-14 05:38:38 +0300

    commit to up gitub

--- calendar-app/src/pages/AddEvent.jsx
+++ calendar-app/src/pages/AddEvent.jsx
@@ -1,3 +1,4 @@
+// src/pages/AddEvent.jsx
 import React from "react";
 
 export default function AddEvent() {
--- calendar-app/src/pages/NotFound.jsx
+++ calendar-app/src/pages/NotFound.jsx
@@ -1,3 +1,4 @@
+// src/pages/NotFound.jsx
 import React from "react";
 
 export default function NotFound() {
